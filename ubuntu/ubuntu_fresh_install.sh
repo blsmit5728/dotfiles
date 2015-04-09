b12e2f5166ed1fe9c723a0c438a4f7bb884dd68e
Brandon Smith <blsmit5728@gmail.com>
2015-04-09 11:38:43 -0400

    Update ubuntu_fresh_install.sh

--- ubuntu/ubuntu_fresh_install.sh
+++ ubuntu/ubuntu_fresh_install.sh
@@ -1,84 +1,53 @@
 #!/bin/bash
 
 function setup_ubuntu {
-	PACKAGES="git vim subversion screen tree"
-	REPOS="repos"
+    PACKAGES=""
+    REPOS="repos"
 
     ARCH=`uname -m`
     X86_64="x86_64"
     X86="x86"
     ARM="armv7l"
     ARM_PI="armv6l"
-
-
     MY_DIR=`dirname $0`
-
-	sudo apt-get -y install "$PACKAGES"
-	if [ -d ~/.ssh ]
+    
+    if [ -d ~/.ssh ]
     then
         echo "SSH Already Created"
     else
         ssh-keygen
     fi
-	sudo apt-get -y install python-software-properties pkg-config
-	sudo apt-get -y install software-properties-common
-
-    # If this is an X86 machine, install XBMC the right way...
-    if [ "${ARCH}" -eq ${X86_64} ]
+    sudo apt-get update
+    sudo apt-get -y install \
+        python-software-properties pkg-config software-properties-common \
+        deluge deluge-web deluged python-setuptools git vim subversion screen tree
+    # setup deluge 
+    sudo cp $MY_DIR/deluge/deluge_default_script /etc/default/deluge-daemon
+    sudo cp $MY_DIR/deluge/deluge_init_d_script /etc/init.d/deluge-daemon 
+    sudo chmod 755 /etc/init.d/deluge-daemon
+    sudo update-rc.d deluge-daemon defaults
+    sudo invoke-rc.d deluge-daemon start
+    sudo mkdir -p /var/log/deluge/daemon
+    sudo mkdir /var/log/deluge/web
+    sudo chmod -R 755 /var/log/deluge
+    sudo chown -R bsmith /var/log/deluge
+    sudo invoke-rc.d deluge-daemon restart
+    # Install Flexget
+    sudo easy_install pip
+    sudo pip install flexget
+    # Setup Deluge Logging
+    cd
+    if [ -d /home/${USER}/repos/ ]
     then
-
-	    UBUNTU_BUILD=`cat /etc/lsb-release | grep DISTRIB_CODENAME | cut -d'=' -f2`
-
-	    case "$UBUNTU_BUILD" in
-		    "saucy")
-			    echo "13.10"
-			    sudo add-apt-repository  ppa:team-xbmc/xbmc-nightly
-			    ;;
-		    "percise")
-			    echo "12.04"
-			    sudo add-apt-repository ppa:team-xbmc/ppa
-			    ;;
-		    *)
-			    echo "Version of Ubuntu unknown!!!"
-			    exit
-			    ;;
-	    esac
-
-	    sudo apt-get update
-        sudo apt-get -y install xbmc xbmc-bin
-	    sudo apt-get -y install deluge deluge-web deluged
-	    sudo cp $MY_DIR/deluge/deluge_default_script /etc/default/deluge-daemon
-	    sudo cp $MY_DIR/deluge/deluge_init_d_script /etc/init.d/deluge-daemon
-	    sudo chmod 755 /etc/init.d/deluge-daemon
-	    sudo update-rc.d deluge-daemon defaults
-	    sudo invoke-rc.d deluge-daemon start
-
+        cd repos
+    else
+        mkdir -v /home/${USER}/repos/
     fi
-
-	# Install Flexget
-	sudo apt-get -y install python-setuptools
-	sudo easy_install pip
-	sudo pip install flexget
-	# Setup Deluge Logging
-	sudo mkdir -p /var/log/deluge/daemon
-	sudo mkdir /var/log/deluge/web
-	sudo chmod -R 755 /var/log/deluge
-	sudo chown -R bsmith /var/log/deluge
-	# restart deluge
-	sudo invoke-rc.d deluge-daemon restart
-
-	cd
-	cd repos
-	git clone https://github.com/blsmit5728/flexget_files.git
-	cd
-	ln -s /home/bsmith/repos/flexget_files/ .flexget
+    git clone https://github.com/blsmit5728/flexget_files.git
+    cd
+    ln -s /home/bsmith/repos/flexget_files/ .flexget
 }
 
-#cd
-#cd repos
-#git clone https://github.com/blsmit5728/dotfiles.git
-#cd dotfiles
-#./install.sh
 
 
 
