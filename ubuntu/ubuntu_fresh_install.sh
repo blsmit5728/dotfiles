8b2e94389d3a163807c212e6b7cc571268736032
Brandon Smith <blsmit5728@gmail.com>
2015-04-21 13:17:23 -0400

    Added some better details to the ubuntu installer.

--- ubuntu/ubuntu_fresh_install.sh
+++ ubuntu/ubuntu_fresh_install.sh
@@ -17,13 +17,15 @@ function setup_ubuntu {
     else
         ssh-keygen
     fi
+    sudo add-apt-repository ppa:deluge-team/ppa
     sudo apt-get update
     sudo apt-get -y install \
         python-software-properties pkg-config software-properties-common \
-        deluge deluge-web deluged python-setuptools git vim subversion screen tree
+        deluge deluge-web deluged python-setuptools git vim subversion screen tree \
+        nmap munin munin-node apache2 
     # setup deluge 
-    sudo cp $MY_DIR/deluge/deluge_default_script /etc/default/deluge-daemon
-    sudo cp $MY_DIR/deluge/deluge_init_d_script /etc/init.d/deluge-daemon 
+    sudo cp $MY_DIR/deluge/default.deluge-daemon /etc/default/deluge-daemon
+    sudo cp $MY_DIR/deluge/init.d.deluge-daemon /etc/init.d/deluge-daemon 
     sudo chmod 755 /etc/init.d/deluge-daemon
     sudo update-rc.d deluge-daemon defaults
     sudo invoke-rc.d deluge-daemon start
@@ -35,13 +37,13 @@ function setup_ubuntu {
     # Install Flexget
     sudo easy_install pip
     sudo pip install flexget
-    # Setup Deluge Logging
     cd
     if [ -d /home/${USER}/repos/ ]
     then
         cd repos
     else
         mkdir -v /home/${USER}/repos/
+        cd /home/${USER}/repos/
     fi
     git clone https://github.com/blsmit5728/flexget_files.git
     cd
